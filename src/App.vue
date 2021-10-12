55ebbbd6f29596499bde58e6df20e5e7d6ce474e
tasya <tasyamaharani77@gmail.com>
2021-10-12 10:12:23 +0700

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <div class="sidebar open">
+    <side-bar></side-bar>
     </div>
-    <router-view/>
+    <div class="home-section">
+    <router-view />
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  .home-section {
+    position: relative;
+    background: #fff;
+    min-height: 100vh;
+    top: 0;
+    left: 78px;
+    width: calc(100% - 78px);
+    transition: all 0.5s ease;
+    z-index: 2;
+  }
 </style>
+<script>
+  import SideBar from "../src/components/sidebar/Sidebar.vue";
+  export default {
+    name: "App",
+    components: {
+      SideBar,
+    },
+  };
+</script>

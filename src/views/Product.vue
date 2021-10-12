ce0582a619caeda604959994155545db0126b7ae
AuliaNurRahmi <aulianurrahmipolines22@gmail.com>
2021-10-12 10:31:59 +0700

    produk

---
+++ src/views/Product.vue
@@ -0,0 +1,194 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <label>PRODUK</label>
+                </div>
+                <div class="col-md-6">
+                    <div class="tambah">
+                        <router-link to="AddProduct"><button type="button">+Tambah</button></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="cariproduk">
+                        <input type="text" class="form-control" placeholder="Cari Produk" aria-label="First name">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="carikategori">
+                        <input type="text" class="form-control" placeholder="Cari Kategori" aria-label="Last name">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+        <div class="container-product2">
+            <div class="col-makan">
+                <h6>BAHAN BANGUNAN</h6>
+                <hr />
+                <div class="row">
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <router-link to="DetailProduct">
+                                <img src="../assets/asbes.jpeg">
+                            </router-link>
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                </div>
+                <h6>BAHAN BANGUNAN</h6>
+                <hr />
+                <div class="row">
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="container-makanan">
+                            <img src="../assets/asbes.jpeg" alt="">
+                            <p class="makanan">Nasi Goreng <br> 15.000</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .col-md-6{
+        padding-left: 0px;
+    }
+    .makanan {
+        font-weight: bold;
+        color: #66DE94;
+        text-align: center;
+
+    }
+
+    hr {
+        border: none;
+        height: 3px;
+        color: white;
+        background-color: white;
+    }
+
+    h6 {
+        font-size: 20px;
+        margin-top: 5px;
+        margin-bottom: 0px;
+        margin-left: 20px;
+        font-family: sans-serif;
+        font-weight: bold;
+        color: white;
+    }
+
+    label {
+        padding-left: 0px;
+        margin-top: 90px;
+        margin-bottom: 15px;
+        color: #66DE94;
+        font-size: 20px;
+        font-weight: bold;
+        font-family: sans-serif;
+    }
+
+    button {
+        margin-top: 90px;
+        border-radius: 15px;
+        background-color: #66DE94;
+        width: 150px;
+        height: 30px;
+        color: white;
+        border-color: transparent;
+    }
+
+    .tambah {
+        text-align: right;
+    }
+
+    .row {
+
+        padding-top: 0px;
+        margin: auto;
+    }
+    .container{
+        padding-left: 0px;
+    }
+    .container-product2 {
+        margin-top: 20px;
+        padding-top: 40px;
+        padding-right: 50px;
+        padding-left: 50px;
+        width: 98%;
+        height: 80%;
+        border-radius: 15px;
+        background-color: #66DE94;
+    }
+
+    img {
+        width: 70px;
+        height: 70px;
+        margin-top: 10px;
+        margin-left: 25px;
+    }
+
+    .container-makanan {
+        margin-top: 12px;
+        margin-bottom: 20px;
+        border-radius: 10px;
+        width: 120px;
+        height: 130px;
+        background-color: white;
+        z-index: 1;
+    }
+</style>

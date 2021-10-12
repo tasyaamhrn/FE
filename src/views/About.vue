71effbeeabce7e4eb6e83ef253ddcc3cf9c09c9f
AuliaNurRahmi <aulianurrahmipolines22@gmail.com>
2021-10-12 13:28:26 +0700

    produk

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
         <h1>TENTANG KAMI</h1>
-        <p class="awal">Aplikasi Kasirin warung adalah aplikasi diperuntukan untuk kasir bagi warung atau 
-            resto yang sedang membutuhkan pencatatan dalam setiap transaksi. Aplikasi di 
-            kembangkan oleh Dokter Apps 
+        <p class="awal">Aplikasi Kasirin Toko adalah aplikasi diperuntukan untuk kasir bagi toko yang sedang 
+          membutuhkan pencatatan dalam setiap transaksi. Aplikasi ini dilengkapi dengan potongan pertransaksi
+          dan stok untuk produk, sehingga anda bisa mudah memonitor produk-produk anda. Aplikasi dikembangkan oleh Dokter Apps 
             <router-link to="`mailto:${dokterapps.mail@gmail.com}`">(dokterapps.mail@gmail.com) </router-link>
             
             

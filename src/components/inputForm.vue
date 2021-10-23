22a0c4ecbf2c4b82f27ecfd543bc2f79a7917c7f
SATRIO <satriotol69@gmail.com>
2021-10-23 22:36:19 +0700

    merapikan register

---
+++ src/components/inputForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-group">
+    <div class="d-flex flex-row align-items-center">
+      <i :class="fa" class="fa-lg me-3 fa-fw"></i>
+      &nbsp;&nbsp;
+      <slot name="form"></slot>
+    </div>
+    <small class="text-danger" v-if="errors">{{ errors[0] }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["fa", "errors", "val"],
+};
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,108 +1,141 @@
 <template>
-    <section class="register">
-        <div class="container ">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-lg-12 col-xl-11">
-                    <div class="card text-black">
-                        <div class="card-body p-md-5">
-                            <div class="row justify-content-center">
-                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+  <section class="register">
+    <div class="container ">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-12 col-xl-11">
+          <div class="card text-black">
+            <div class="card-body p-md-5">
+              <div class="row justify-content-center">
+                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                  <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                    Daftar
+                  </p>
 
-                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Daftar</p>
-
-                                    <form class="mx-1 mx-md-4">
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            
-                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                             &nbsp;&nbsp;
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="text" id="form3Example1c" class="form-control"
-                                                    placeholder="Email Anda.." />
-                                                <!-- <label class="form-label" for="form3Example1c">Your Name</label> -->
-                                            </div>
-                                        </div>
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                              &nbsp;&nbsp;
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="email" id="form3Example3c" class="form-control"
-                                                    placeholder="Username" />
-                                                <!-- <label class="form-label" for="form3Example3c">Your Email</label> -->
-                                            </div>
-                                        </div>
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-store-alt fa-lg me-3 fa-fw"></i>
-                                              &nbsp;&nbsp;
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="form3Example4c" class="form-control"
-                                                    placeholder="Nama Toko Anda.." />
-                                                <!-- <label class="form-label" for="form3Example4c">Password</label> -->
-                                            </div>
-                                        </div>
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                              &nbsp;&nbsp;
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="form3Example4c" class="form-control"
-                                                    placeholder="Password.." />
-                                                <!-- <label class="form-label" for="form3Example4c">Password</label> -->
-                                            </div>
-                                        </div>
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-phone-alt fa-lg me-3 fa-fw"></i>
-                                              &nbsp;&nbsp;
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="form3Example4c" class="form-control"
-                                                    placeholder="Nomor Telepon.." />
-                                                <!-- <label class="form-label" for="form3Example4c">Password</label> -->
-                                            </div>
-                                        </div>
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-map-marker-alt fa-lg me-3 fa-fw"></i>
-                                              &nbsp;&nbsp;
-                                            <div class="form-outline flex-fill mb-0">
-                                                <textarea  id="form3Example4c" class="form-control"
-                                                    placeholder="Alamat Toko Anda.." />
-                                                <!-- <label class="form-label" for="form3Example4c">Password</label> -->
-                                            </div>
-                                        </div>
-                                       
-
-                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <router-link to="/Login"><button type="button" class="btn btn-success btn-lg">Daftar</button></router-link>
-                                        </div>
-
-                                    </form>
-
-                                </div>
-                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                                    <img src="../assets/shop.jpg" class="img-fluid" alt="Sample image">
-
-                                </div>
-                            </div>
-                        </div>
+                  <form class="mx-1 mx-md-4">
+                    <input-form fa="fas fa-user" :errors="errors.name">
+                      <template v-slot:form>
+                        <input
+                          type="text"
+                          placeholder="Masukkan Nama"
+                          v-model="form.name"
+                          class="form-control"
+                        />
+                      </template>
+                    </input-form>
+                    <input-form fa="fas fa-envelope" :errors="errors.email">
+                      <template v-slot:form>
+                        <input
+                          type="email"
+                          placeholder="Masukkan Email"
+                          v-model="form.email"
+                          class="form-control"
+                        />
+                      </template>
+                    </input-form>
+                    <input-form fa="fas fa-lock" :errors="errors.password">
+                      <template v-slot:form>
+                        <input
+                          type="password"
+                          placeholder="Masukkan Password"
+                          v-model="form.password"
+                          class="form-control"
+                        />
+                      </template>
+                    </input-form>
+                    <input-form fa="fas fa-phone" :errors="errors.phone">
+                      <template v-slot:form>
+                        <input
+                          type="number"
+                          placeholder="Masukkan Nomor Telepon"
+                          v-model="form.phone"
+                          class="form-control"
+                        />
+                      </template>
+                    </input-form>
+                    <input-form
+                      fa="fas fa-map-marker-alt"
+                      :errors="errors.address"
+                    >
+                      <template v-slot:form>
+                        <textarea
+                          class="form-control"
+                          placeholder="Masukkan Alamat Toko Anda"
+                          v-model="form.address"
+                        />
+                      </template>
+                    </input-form>
+                    <div
+                      class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                    >
+                      <button
+                        @click="register"
+                        type="button"
+                        class="btn btn-success btn-lg"
+                      >
+                        Daftar
+                      </button>
                     </div>
+                  </form>
                 </div>
+                <div
+                  class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2"
+                >
+                  <img
+                    src="../assets/shop.jpg"
+                    class="img-fluid"
+                    alt="Sample image"
+                  />
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 <style scoped>
-    .register {
-        height: 100vh;
-        background-color: #eee;
-    }
+.register {
+  height: 100vh;
+  background-color: #eee;
+}
 
-    .container {
-        height: 100%;
-    }
+.container {
+  height: 100%;
+}
 
-    .text-black {
-        border-radius: 25px;
-    }
-
+.text-black {
+  border-radius: 25px;
+}
 </style>
+<script>
+import axios from "axios";
+import InputForm from "../components/inputForm.vue";
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        address: "",
+        gender: "Male",
+        avatar: "",
+        phone: "",
+        password: "",
+      },
+      errors: [],
+    };
+  },
+  components: { InputForm },
+  methods: {
+    register() {
+      axios
+        .post("https://api-kasirin.jaggs.id/api/register", this.form)
+        .then((res) => console.log(res))
+        .catch((err) => {
+          this.errors = err.response.data;
+        });
+    },
+  },
+};
+</script>

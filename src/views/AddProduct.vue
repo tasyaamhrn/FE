885924400e776cb998c8f6b0ef2ecca6b80335c8
SATRIO <satriotol69@gmail.com>
2021-10-28 10:47:36 +0700

    product

--- src/views/AddProduct.vue
+++ src/views/AddProduct.vue
@@ -1,114 +1,110 @@
 <template>
-    <div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <button 
-                        @click="product"
-                        type="button"
-                        class="btn btn-success btn-lg"
-                      >
-                        TAMBAH
-                      </button>
-                    <!-- <label>TAMBAH PRODUK</label> -->
-                </div>
-                <div class="col-md-6">
-                    <div class="kembali">
-                        <router-link to="product">
-                            <button type="button">
-                                &#8592; Kembali
-                            </button>
-                        </router-link>
-                    </div>
-                </div>
-            </div>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <button @click="product" type="button" class="btn btn-success btn-lg">
+            TAMBAH
+          </button>
+          <!-- <label>TAMBAH PRODUK</label> -->
         </div>
-        <div class="container">
-            <p class="judul">
-                Nama Produk/Layanan
-            </p>
-            <input-form :errors="errors.name">
-            <template v-slot:form>
-                <input
-                    class="form-control"
-                    id="namaproduk"
-                    type="text"
-                    placeholder="Masukkan Nama Barang"
-                    v-model="form.name"
-                />
-            </template>  
-            </input-form> 
-            <p class="judul">
-                Nama Kategori
-            </p>
-            <input-form :errors="errors.category_id">
-            <template v-slot:form>
-                <input
-                    class="form-control"
-                    id="kategoriproduk"
-                    type="int"
-                    placeholder="Pilih Kategori Barang"
-                    v-model="form.category_id"
-                />
-            </template>  
-            </input-form>
-            <p class="judul">
-                Foto Produk/Layanan
-            </p>
-            <input-form :errors="errors.image">
-            <template v-slot:form>
-                <input
-                    class="form-control"
-                    id="fotoproduk"
-                    type="file"
-                    placeholder="Masukkan Foto Barang"
-                />
-            </template>  
-            </input-form>
-            <p class="judul">
-                Harga Produk/Layanan
-            </p>
-            <input-form :errors="errors.price">
-            <template v-slot:form>
-                <input
-                    class="form-control"
-                    id="namaproduk"
-                    type="text"
-                    placeholder="Masukkan Harga Barang"
-                    v-model="form.price"
-                />
-            </template>  
-            </input-form>
-            <p class="judul">
-                Stock Produk/Layanan
-            </p>
-            <input-form :errors="errors.stock">
-            <template v-slot:form>
-                <input
-                    class="form-control"
-                    id="stockproduk"
-                    type="int"
-                    placeholder="Masukkan Stock Barang"
-                    v-model="form.stock"
-                />
-            </template>  
-            </input-form>
-            <p class="judul">
-                Barcode Produk/Layanan
-            </p>
-            <input-form :errors="errors.barcode">
-            <template v-slot:form>
-                <input
-                    class="form-control"
-                    id="barcodeproduk"
-                    type="int"
-                    placeholder="Masukkan Nomor Barcode"
-                    v-model="form.barcode"
-                />
-            </template>  
-            </input-form>
-            
-            <!-- <input-form :errors="errors.category_id">
+        <div class="col-md-6">
+          <div class="kembali">
+            <router-link to="product">
+              <button type="button">
+                &#8592; Kembali
+              </button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="judul">
+        Nama Produk/Layanan
+      </p>
+      <input-form :errors="errors.name">
+        <template v-slot:form>
+          <input
+            class="form-control"
+            id="namaproduk"
+            type="text"
+            placeholder="Masukkan Nama Barang"
+            v-model="form.name"
+          />
+        </template>
+      </input-form>
+      <p class="judul">
+        Nama Kategori
+      </p>
+      <input-form :errors="errors.category_id">
+        <template v-slot:form>
+          <input
+            class="form-control"
+            id="kategoriproduk"
+            type="int"
+            placeholder="Pilih Kategori Barang"
+            v-model="form.category_id"
+          />
+        </template>
+      </input-form>
+      <p class="judul">
+        Foto Produk/Layanan
+      </p>
+      <input-form :errors="errors.image">
+        <template v-slot:form>
+          <input
+            class="form-control"
+            id="fotoproduk"
+            type="file"
+            placeholder="Masukkan Foto Barang"
+          />
+        </template>
+      </input-form>
+      <p class="judul">
+        Harga Produk/Layanan
+      </p>
+      <input-form :errors="errors.price">
+        <template v-slot:form>
+          <input
+            class="form-control"
+            id="namaproduk"
+            type="text"
+            placeholder="Masukkan Harga Barang"
+            v-model="form.price"
+          />
+        </template>
+      </input-form>
+      <p class="judul">
+        Stock Produk/Layanan
+      </p>
+      <input-form :errors="errors.stock">
+        <template v-slot:form>
+          <input
+            class="form-control"
+            id="stockproduk"
+            type="int"
+            placeholder="Masukkan Stock Barang"
+            v-model="form.stock"
+          />
+        </template>
+      </input-form>
+      <p class="judul">
+        Barcode Produk/Layanan
+      </p>
+      <input-form :errors="errors.barcode">
+        <template v-slot:form>
+          <input
+            class="form-control"
+            id="barcodeproduk"
+            type="int"
+            placeholder="Masukkan Nomor Barcode"
+            v-model="form.barcode"
+          />
+        </template>
+      </input-form>
+
+      <!-- <input-form :errors="errors.category_id">
             <template v-slot:form>
                 <select
                     class="form-control"
@@ -120,50 +116,49 @@
             </template>  
             </input-form> -->
 
-            <!-- <select class="col-lg-12" aria-label=".form-select-lg example">
+      <!-- <select class="col-lg-12" aria-label=".form-select-lg example">
                 <option selected>Pilih Kategori Produk..</option>
                 <option value="1">Barang Pokok</option>
                 <option value="2">Barang Impulsif</option>
                 <option value="3">Barang Darurat</option>
             </select> -->
-            
-        </div>
     </div>
+  </div>
 </template>
 <style scoped>
-    .col-lg-12{
-        height: 34px;
-    }
-    label {
-        padding-top: 90px;
-        margin-bottom: 15px;
-        color: #4CAF50;
-        font-size: 20px;
-        font-weight: bold;
-        font-family: sans-serif;
-    }
+.col-lg-12 {
+  height: 34px;
+}
+label {
+  padding-top: 90px;
+  margin-bottom: 15px;
+  color: #4caf50;
+  font-size: 20px;
+  font-weight: bold;
+  font-family: sans-serif;
+}
 
-    button {
-        margin-top: 90px;
-        border-radius: 15px;
-        background-color: #376CAF;
-        width: 150px;
-        height: 40px;
-        color: white;
-        border-color: transparent;
-    }
+button {
+  margin-top: 90px;
+  border-radius: 15px;
+  background-color: #376caf;
+  width: 150px;
+  height: 40px;
+  color: white;
+  border-color: transparent;
+}
 
-    .kembali {
-        text-align: right;
-    }
+.kembali {
+  text-align: right;
+}
 
-    .judul {
-        color: #4CAF50;
-        font-family: sans-serif;
-        font-size: 18px;
-        padding-top: 10px;
-        margin-bottom: 5px;
-    }
+.judul {
+  color: #4caf50;
+  font-family: sans-serif;
+  font-size: 18px;
+  padding-top: 10px;
+  margin-bottom: 5px;
+}
 </style>
 <script>
 import axios from "axios";
@@ -174,7 +169,7 @@ export default {
       form: {
         name: "",
         category_id: "",
-        image:"",
+        image: "",
         price: "",
         stock: "",
         barcode: "",
@@ -187,9 +182,11 @@ export default {
     product() {
       axios
         .post("https://api-kasirin.jaggs.id/api/product/store", this.form)
-        .then((res) => console.log(res));
-        this.$router.push({
-          name:'Product'
+        .then((res) => {
+          console.log(res);
+          this.$router.push({
+            name: "Product",
+          });
         })
         .catch((err) => {
           this.errors = err.response.data;
@@ -198,4 +195,3 @@ export default {
   },
 };
 </script>
-
